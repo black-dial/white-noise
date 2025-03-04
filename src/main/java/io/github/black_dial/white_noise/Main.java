5f3517ec3b4b4a2fb11ebc44dd85060dce7cd1b5
Andres Romero <romero.ramos@proton.me>
2025-03-03 22:12:35 -0600

    RED: support cli interaction

--- src/main/java/io/github/black_dial/white_noise/Main.java
+++ src/main/java/io/github/black_dial/white_noise/Main.java
@@ -1,7 +1,16 @@
 package io.github.black_dial.white_noise;
 
+import io.github.black_dial.white_noise.cli.Session;
+
 public class Main {
+    public enum VERB {ADD, LIST, CHECK}
+
+    private static Session session;
+
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    static Session getSession() {return null;}
+    static void setSession(Session session) {}
 }
---
+++ src/test/java/io/github/black_dial/white_noise/MainTest.java
@@ -0,0 +1,70 @@
+package io.github.black_dial.white_noise;
+
+import io.github.black_dial.white_noise.cli.Session;
+import io.github.black_dial.white_noise.data.TaskTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class MainTest {
+    public static final String UNKNOWN_COMMAND = "unknown";
+
+    @Mock
+    private Session session;
+
+    private void verifyNoSessionInteraction() {
+        verify(session, never()).add(any());
+        verify(session, never()).list(any());
+        verify(session, never()).check(any());
+    }
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        Main.setSession(session);
+    }
+
+    @Test
+    void shouldCallAdd() {
+        Main.main(new String[]{Main.VERB.ADD.toString().toLowerCase(), TaskTest.TEST_DESC});
+
+        ArgumentCaptor<String[]> captor = ArgumentCaptor.forClass(String[].class);
+        verify(session).add(captor.capture());
+        assertArrayEquals(new String[]{TaskTest.TEST_DESC}, captor.getValue());
+    }
+
+    @Test
+    void shouldCallList() {
+        Main.main(new String[]{Main.VERB.LIST.toString().toLowerCase()});
+
+        verify(session).list(new String[]{});
+    }
+
+    @Test
+    void shouldCallCheck() {
+        Main.main(new String[]{Main.VERB.LIST.toString().toLowerCase(), TaskTest.TEST_ID.toString()});
+
+        ArgumentCaptor<String[]> captor = ArgumentCaptor.forClass(String[].class);
+        verify(session).list(captor.capture());
+        assertArrayEquals(new String[]{TaskTest.TEST_ID.toString()}, captor.getValue());
+    }
+
+    @Test
+    void shouldShowUsageOnNoArgs() {
+        Main.main(new String[]{});
+
+        verifyNoSessionInteraction();
+    }
+
+    @Test
+    void shouldHandleUnknownCommand() {
+        Main.main(new String[]{UNKNOWN_COMMAND});
+
+        verifyNoSessionInteraction();
+    }
+}
